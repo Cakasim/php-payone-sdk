9ab3be2c25f04b69e5cd95e34449c30965ab80a5
Fabian Böttcher <me@cakasim.de>
2019-12-01 17:53:10 +0100

    Fix API format encoding

--- src/Api/Format/Encoder.php
+++ src/Api/Format/Encoder.php
@@ -17,40 +17,6 @@ class Encoder implements EncoderInterface
      */
     public function encode(array $data): string
     {
-        // Transform nested parameters.
-        $data = static::transformData($data, null);
-
-        // Make param=value lines array.
-        $result = [];
-        foreach ($data as $key => $value) {
-            $result[] = "{$key}=" . rawurlencode((string) $value);
-        }
-
-        // Combine and return the lines.
-        return join("\n", $result);
-    }
-
-    /**
-     * Transform nested data.
-     *
-     * @param array $data The data to transform.
-     * @param string|null $prefix The prefix to prepend to keys.
-     * @return array The transformed data.
-     */
-    protected static function transformData(array $data, ?string $prefix): array
-    {
-        $result = [];
-
-        foreach ($data as $key => $value) {
-            // Prepend prefix to current key.
-            $key = $prefix ? "{$prefix}[{$key}]" : $key;
-
-            // If the value is an array, process this value recursively.
-            is_array($value)
-                ? $result = array_merge($result, static::transformData($value, $key))
-                : $result[$key] = $value;
-        }
-
-        return $result;
+        return http_build_query($data);
     }
 }
