b0e793ffb416768b868d74911e92b4345db1a2e0
Fabian Böttcher <me@cakasim.de>
2020-01-19 17:35:39 +0100

    Use Psr\Log\AbstractLogger

--- src/Log/AbstractLogger.php
+++ src/Log/AbstractLogger.php
@@ -4,78 +4,12 @@ declare(strict_types=1);
 
 namespace Cakasim\Payone\Sdk\Log;
 
-use Psr\Log\LoggerInterface;
-use Psr\Log\LogLevel;
-
 /**
  * Implements base functionality of the LoggerInterface.
  *
  * @author Fabian Böttcher <me@cakasim.de>
  * @since 0.1.0
  */
-abstract class AbstractLogger implements LoggerInterface
+abstract class AbstractLogger extends \Psr\Log\AbstractLogger
 {
-    /**
-     * @inheritDoc
-     */
-    public function emergency($message, array $context = []): void
-    {
-        $this->log(LogLevel::EMERGENCY, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function alert($message, array $context = []): void
-    {
-        $this->log(LogLevel::ALERT, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function critical($message, array $context = []): void
-    {
-        $this->log(LogLevel::CRITICAL, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function error($message, array $context = []): void
-    {
-        $this->log(LogLevel::ERROR, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function warning($message, array $context = []): void
-    {
-        $this->log(LogLevel::WARNING, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function notice($message, array $context = []): void
-    {
-        $this->log(LogLevel::NOTICE, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function info($message, array $context = []): void
-    {
-        $this->log(LogLevel::INFO, $message, $context);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function debug($message, array $context = []): void
-    {
-        $this->log(LogLevel::DEBUG, $message, $context);
-    }
 }
