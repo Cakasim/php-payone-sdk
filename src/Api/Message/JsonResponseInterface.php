25d74cbca3f53846eabc6adeea7973b5acc23d3c
Fabian Böttcher <me@cakasim.de>
2020-01-27 20:05:45 +0100

    Add interface for JSON API responses

---
+++ src/Api/Message/JsonResponseInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cakasim\Payone\Sdk\Api\Message;
+
+/**
+ * Represents a JSON API response.
+ *
+ * @author Fabian Böttcher <me@cakasim.de>
+ * @since 0.1.0
+ */
+interface JsonResponseInterface extends ResponseInterface
+{
+}
